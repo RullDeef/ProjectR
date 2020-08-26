35429241843494cd02e583a51327d2b4d8027151
Rull Deef <deeroll666@gmail.com>
2020-08-26 13:06:43 +0300

    add test environment

---
+++ Assets/Scripts/Core/RT/Camera/RTCameraController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class RTCameraController : MonoBehaviour
+{
+    public Transform followingObject;
+    public float smoothness = 0.2f;
+
+    public float observingAngle = 45.0f; // degrees
+    public float observingDistance = 10.0f;
+
+    public float observingYaw = 0.0f;
+
+
+    private void FixedUpdate()
+    {
+        if (followingObject != null)
+        {
+            UpdateCameraPosition();
+            UpdateCameraRotation();
+            UpdateCameraZoom();
+        }
+    }
+
+    private void UpdateCameraRotation()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            float dx = Input.GetAxis("Mouse X");
+            transform.RotateAround(followingObject.position, Vector3.up, dx);
+            observingYaw += dx;
+
+            float dy = Input.GetAxis("Mouse Y");
+            observingAngle -= dy;
+            observingAngle = Mathf.Clamp(observingAngle, 15.0f, 75.0f);
+        }
+
+        transform.LookAt(followingObject);
+    }
+
+    private void UpdateCameraPosition()
+    {
+        float angle = observingAngle * Mathf.Deg2Rad;
+
+        float backStepSize = observingDistance * Mathf.Cos(angle);
+        float upStepSize = observingDistance * Mathf.Sin(angle);
+
+        Vector3 backStep = backStepSize * followingObject.TransformDirection(Vector3.back);
+        backStep = Quaternion.AngleAxis(observingYaw, Vector3.up) * backStep;
+        Vector3 upStep = upStepSize * Vector3.up;
+
+        Vector3 desiredCameraPosition = followingObject.position + backStep + upStep;
+
+        Vector3 delta = desiredCameraPosition - transform.position;
+
+        transform.position += 1.0f / smoothness * delta * Time.fixedDeltaTime;
+    }
+
+    private void UpdateCameraZoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        observingDistance -= 4.0f * scroll;
+        observingDistance = Mathf.Max(2.0f, observingDistance);
+    }
+}
---
+++ Assets/Scripts/Core/RT/Player/RTPlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Core.RT.Player
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class RTPlayerController : MonoBehaviour
+    {
+        public float movementSpeed = 5.0f;
+        public float jumpForce = 10.0f;
+        public RTCameraController cameraController;
+
+        private bool isGrounded = false;
+
+        private Rigidbody body;
+
+        void Awake()
+        {
+            body = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            float dHor = Input.GetAxis("Horizontal");
+            float dVer = Input.GetAxis("Vertical");
+
+            Vector3 delta = new Vector3(dHor, 0, dVer);
+            delta = transform.TransformDirection(delta);
+            delta = Quaternion.AngleAxis(cameraController.observingYaw, Vector3.up) * delta;
+            delta *= movementSpeed;
+
+            delta.y = body.velocity.y;
+            body.velocity = delta;
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                body.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            }
+        }
+
+        void OnCollisionEnter(Collision other)
+        {
+            isGrounded = true;
+        }
+
+        void OnCollisionExit(Collision other)
+        {
+            isGrounded = false;
+        }
+    }
+}
