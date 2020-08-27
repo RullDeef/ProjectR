a438fd8f14d1a6d7beee6984e1101cada3f1d789
Rull Deef <deeroll666@gmail.com>
2020-08-27 15:01:32 +0300

    link pause menu

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+    public RTUI RTUI;
+    public bool isPaused { get; private set; }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public static void PauseGame()
+    {
+        instance.isPaused = true;
+        instance.RTUI.OpenPauseMenu();
+    }
+
+    public static void ResumeGame()
+    {
+        instance.RTUI.ClosePauseMenu();
+        instance.isPaused = false;
+    }
+}
---
+++ Assets/Scripts/Core/UI/RTUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class RTUI : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void OpenPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    public void ClosePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        GameManager.PauseGame();
+    }
+
+    public void ResumeGame()
+    {
+        GameManager.ResumeGame();
+    }
+}
