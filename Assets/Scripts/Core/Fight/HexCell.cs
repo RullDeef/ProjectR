4c3ed3855b0a93f3115d6964515552d9eda71eaf
Rull Deef <deeroll666@gmail.com>
2020-08-28 22:55:46 +0300

    add cineware package

---
+++ Assets/Scripts/Core/Fight/HexCell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+    private List<HexCell> neighbors;
+
+    private void Awake()
+    {
+        FindNeighbors();
+    }
+
+    private void FindNeighbors()
+    {
+        neighbors = new List<HexCell>();
+
+        foreach (HexCell cell in transform.parent.GetComponentsInChildren<HexCell>())
+        {
+            if (cell != this && Vector3.Distance(transform.position, cell.transform.position) <= 2.1f)
+            {
+                neighbors.Add(cell.GetComponent)
+            }
+        }
+    }
+}
