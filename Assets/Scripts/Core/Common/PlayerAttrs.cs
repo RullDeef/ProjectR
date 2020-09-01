f2c8935b4eee4ab97f2eb54d625d8b74e4544dd9
Rull Deef <deeroll666@gmail.com>
2020-09-01 19:40:41 +0300

    add stats & player attrs

---
+++ Assets/Scripts/Core/Common/PlayerAttrs.cs
@@ -0,0 +1,10 @@
+namespace Core.Common
+{
+    /**
+     *  @brief Улучшаемые атрибуты главного героя.
+     */
+    public class PlayerAttrs
+    {
+        public float atackPower; // primary atack power
+    }
+}
---
+++ Assets/Scripts/Core/Common/UnitStats.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Core.Common
+{
+    [CreateAssetMenu(fileName = "UnitStats", menuName = "ProjectR/UnitStats", order = 0)]
+    public class UnitStats : ScriptableObject
+    {
+        public float healthPoints;
+        public float manaPoints;
+        public float enegry;
+        public float defence;
+    }
+}
